3f8f798c6ff43fdde95ee5177fcda8cec58d5cf0
gustavojcorrea <gcorr003@ucr.edu>
2018-08-28 17:09:00 -0700

    added ros_openroach package with working ros_openroach node

---
+++ catkin_ws-8-23-18-5-03-PM/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src
+# Build directory: /home/ubuntu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(edumip_msgs)
+subdirs(edumip_balance_ros)
+subdirs(ros_openroach)
+subdirs(ros-tag-tracking)
+subdirs(usb_cam)
---
+++ catkin_ws-8-23-18-5-03-PM/build/ros_openroach/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src/ros_openroach
+# Build directory: /home/ubuntu/catkin_ws/build/ros_openroach
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
