de49a02ef8a34be00e3622695d44fe100b9cdcb3
Gustavo Correa <gcorr003@ucr.edu>
2018-08-28 17:16:48 -0700

    included catkin_ws for vm running ar-tag-demo node

---
+++ catkin_ws/build/ros-tag-tracking/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/catkin_ws/src/ros-tag-tracking
+# Build directory: /home/parallels/catkin_ws/build/ros-tag-tracking
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
