cdc5935db52122f0bff207604199403c627d1793
Gustavo Correa <gcorr003@ucr.edu>
2018-08-29 20:10:50 -0700

    added linux vm catkin_ws

---
+++ catkin_ws/build/vision_visp/visp_tracker/CMakeFiles/visp_tracker_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visp_tracker_generate_messages_lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/MovingEdgeSettings.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/KltPoints.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/MovingEdgeSites.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/KltPoint.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/TrackerSettings.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/KltSettings.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/msg/MovingEdgeSite.lisp"
+  "/home/parallels/catkin_ws/devel/share/common-lisp/ros/visp_tracker/srv/Init.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/visp_tracker_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/devel/share/gennodejs/ros/visp_camera_calibration/msg/_index.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+let CalibPointArray = require('./CalibPointArray.js');
+let ImageAndPoints = require('./ImageAndPoints.js');
+let ImagePoint = require('./ImagePoint.js');
+let CalibPoint = require('./CalibPoint.js');
+
+module.exports = {
+  CalibPointArray: CalibPointArray,
+  ImageAndPoints: ImageAndPoints,
+  ImagePoint: ImagePoint,
+  CalibPoint: CalibPoint,
+};
