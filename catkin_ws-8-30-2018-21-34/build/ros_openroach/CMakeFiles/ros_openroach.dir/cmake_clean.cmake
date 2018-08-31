7a2bd5c9975dc7d40b67908cc3fe3b9b5f2eedd9
gustavojcorrea <gcorr003@ucr.edu>
2018-08-31 13:55:42 -0700

    openroach system test 1

---
+++ catkin_ws-8-30-2018-21-34/build/ros_openroach/CMakeFiles/ros_openroach.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_openroach.dir/src/ros_openroach_node.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/ros_openroach/ros_openroach.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/ros_openroach/ros_openroach"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros_openroach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws-8-30-2018-21-34/src/ros_openroach/include/openroach_class.h
@@ -0,0 +1,63 @@
+#ifndef OPENROACH_CLASS_H_
+#define OPENROACH_CLASS_H_
+
+//some generically useful stuff to include...
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include <ros/ros.h> //ALWAYS need to include this
+#include "geometry_msgs/PoseStamped.h"
+//#include <tf/transform_broadcaster.h>
+
+//message types used in this example code;  include more message types, as needed
+#include <std_msgs/Bool.h> 
+#include <std_msgs/Float32.h>
+
+
+// define a class, including a constructor, member variables and member functions
+class OpenRoachClass
+{
+public:
+    OpenRoachClass(ros::NodeHandle* nodehandle); //"main" will need to instantiate a ROS nodehandle, then pass it to the constructor
+    // may choose to define public methods or public variables, if desired
+private:
+    // put private member data here;  "private" data will only be available to member functions of this class;
+    ros::NodeHandle nh_; // we will need this, to pass between "main" and constructor
+    // some objects to support subscriber, service, and publisher
+    ros::Subscriber tag_pose_sub; //these will be set up within the class constructor, hiding these ugly details
+    ros::ServiceServer minimal_service_;
+    ros::Publisher  minimal_publisher_;
+    
+    double val_to_remember_; // member variables will retain their values even as callbacks come and go
+    float x_pos_tag;
+    float y_pos_tag;
+    float z_pos_tag;
+    double roll_tag;
+    double pitch_tag;
+    double yaw_tag;
+    //tf::Transform transform_tag;
+ 
+    float x_pos_cam;
+    float y_pos_cam;
+    float z_pos_cam;
+    //tf::Transform transform_cam;
+    float DUTY_MAX;
+    float DUTY_L;
+    float DUTY_R;
+
+    float prevXerror;
+
+    std::string WORLD_FRAME_NAME;
+    std::string BODY_FRAME_NAME;    
+    // member methods as well:
+    void initializeSubscribers(); // we will define some helper methods to encapsulate the gory details of initializing subscribers, publishers and services
+    void initializePublishers();
+    void initializeServices();
+   
+    void tag_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& pose); 
+ }; // note: a class definition requires a semicolon at the end of the definition
+
+#endif  // this closes the header-include trick...ALWAYS need one of these to match #ifndef
+
